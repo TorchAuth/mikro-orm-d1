b58dff89a6e003e7b6b053b03127eb84da46ac09
Bruce Harrison <leeharrison1984@gmail.com>
2024-07-22 11:49:31 -0500

    cleanup

--- src/D1Connection.ts
+++ src/D1Connection.ts
@@ -1,14 +1,14 @@
-import { BaseSqliteConnection } from "@mikro-orm/knex";
+import { BetterSqliteConnection } from "@mikro-orm/better-sqlite";
 import Client_D1 from "@torchauth/knex-cloudflare-d1";
 
-export class D1Connection extends BaseSqliteConnection {
+export class D1Connection extends BetterSqliteConnection {
   override async createKnex() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     this.client = this.createKnexClient(Client_D1 as any);
+    this.connected = true;
   }
 
   override async connect() {
     this.createKnex();
-    this.connected = true;
   }
 }
