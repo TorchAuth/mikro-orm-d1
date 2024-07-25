c428a542b5482ff95e4cfbaf886352a12108f635
Bruce Harrison <leeharrison1984@gmail.com>
2024-07-24 23:04:37 -0500

    wip

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,12 +1,15 @@
-import { MikroORM } from '../../dist/mikro-orm-d1';
+import { GeneratedCacheAdapter, MikroORM } from '../../dist/mikro-orm-d1';
 import { User } from './test.entity';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const mikro = await MikroORM.init({
 			debug: true,
-			dbName: 'd1',
-			entities: [User],
+			metadataCache: {
+				enabled: true,
+				adapter: GeneratedCacheAdapter,
+				options: { data: require('./meta.json') },
+			},
 			driverOptions: {
 				connection: {
 					database: env.MY_DB,
